705e37afe536706c4f46adf0d2720bfa7af5ee6b
Dhruv <samantdhruv@gmail.com>
2021-09-09 23:09:00 +0530

    added previous entries

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-// document.getElementById("count-el").innerText = 5
 
-let countEl = document.getElementById("count-el")
-let count = 0
-
-function increment() {
-    count = count + 1
-    countEl.innerText = count
-}
-
-function save() {
-    console.log(count)
-}
+ let saveEl = document.getElementById("save-el")
+ let countEl = document.getElementById("count-el")
+ let count = 0
+ 
+ function increment() {
+     count += 1
+     countEl.innerText = count
+ }
+ 
+ function save() {
+     let countStr = count + " - "
+     saveEl.innerText += countStr
+ }
